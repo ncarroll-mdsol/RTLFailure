ce579c6e933e94186c3c407e5ac982cec25a4b7c
Nolan Carroll <ncarroll@mdsol.com>
2015-09-04 15:34:24 -0400

    initial commit

---
+++ RTLFailure/ExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExampleViewController.h
+//  RTLFailure
+//
+//  Created by Nolan Carroll on 9/4/15.
+//  Copyright (c) 2015 Medidata Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExampleViewController : UIViewController
+
+@end
---
+++ RTLFailure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTLFailure
+//
+//  Created by Nolan Carroll on 9/4/15.
+//  Copyright (c) 2015 Medidata Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
